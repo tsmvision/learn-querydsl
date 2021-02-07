eb88c56af9ba39037d205a7a53f8a79923f5f717
lukelee <lukelee01@gmail.com>
2021-02-07 17:59:10 -0500

    first commit

---
+++ src/main/java/org/javastudy/learnquerydsl/LearnQuerydslApplication.java
@@ -0,0 +1,13 @@
+package org.javastudy.learnquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javastudy/learnquerydsl/LearnQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javastudy.learnquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
