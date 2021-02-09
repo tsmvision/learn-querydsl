3818f837eb2ad0db6e2f2f07ca994438580ce572
lukelee <lukelee01@gmail.com>
2021-02-08 21:17:24 -0500

    create test case for QuertyDSL Projection

--- src/main/java/org/javastudy/learnquerydsl/dto/MemberDto.java
+++ src/main/java/org/javastudy/learnquerydsl/dto/MemberDto.java
@@ -12,7 +12,7 @@ public class MemberDto {
     private String username;
     private int age;
 
-    @QueryProjection
+//    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
